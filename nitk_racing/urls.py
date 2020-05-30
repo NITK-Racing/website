70f530c37e864254b2faf5a224509c7eb39bfc47
Harshalzzzzzzz <harshal2019@gmail.com>
2020-05-30 15:53:19 +0530

    XYZ

--- nitk_racing/urls.py
+++ nitk_racing/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mysite.urls'))
+    path('', include('mysite.urls')),
+    path('nitkracing_ev/', include('nitkracing_ev.urls'))
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
